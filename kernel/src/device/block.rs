800dbef95c9414a1862dfab4dff4ec1f4704a559
H4n-uL <jun061119@gmail.com>
2025-07-22 09:41:59 +0900

    ADD :: Added BLOCK_DEVICES static vec

--- kernel/src/device/block.rs
+++ kernel/src/device/block.rs
@@ -1,8 +1,11 @@
-use alloc::string::String;
+use alloc::{boxed::Box, string::String, vec::Vec};
+use spin::Mutex;
 
-pub trait BlockDevice {
+pub trait BlockDevice: Send + Sync {
     fn block_size(&self) -> usize;
     fn block_count(&self) -> usize;
     fn read(&self, lba: u64, buffer: &mut [u8]) -> Result<(), String>;
     fn write(&self, lba: u64, buffer: &[u8]) -> Result<(), String>;
 }
+
+pub static BLOCK_DEVICES: Mutex<Vec<Box<dyn BlockDevice>>> = Mutex::new(Vec::new());
