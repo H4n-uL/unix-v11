af77c8e0f444313af54b6f52e7a272a6804fbf85
H4n-uL <jun061119@gmail.com>
2025-09-09 23:02:27 +0900

    UPDATE :: Added Directory support

--- kernel/src/filesys/mod.rs
+++ kernel/src/filesys/mod.rs
@@ -1,52 +1,186 @@
-use crate::{printlnk, ram::dump_bytes};
-use alloc::{collections::btree_map::BTreeMap, string::String, vec::Vec};
+mod vfn;
+
+use crate::{filesys::vfn::{FMeta, FType, VirtFNode}, ram::dump_bytes};
+use alloc::{collections::btree_map::BTreeMap, string::String, sync::Arc, vec::Vec};
+use spin::Mutex;
+
+struct VirtFile {
+    vfd: Mutex<VFileData>
+}
+
+struct VFileData {
+    meta: FMeta,
+    data: Vec<u8>
+}
+
+impl VirtFile {
+    pub fn new() -> Self {
+        return Self {
+            vfd: Mutex::new(VFileData {
+                meta: FMeta::default(FType::Regular),
+                data: Vec::new()
+            })
+        };
+    }
+}
+
+impl VirtFNode for VirtFile {
+    fn meta(&self) -> FMeta {
+        return self.vfd.lock().meta.clone();
+    }
+
+    fn read(&self, buf: &mut [u8], offset: u64) -> bool {
+        let data = &self.vfd.lock().data;
+        let offset = offset as usize;
+        if offset >= data.len() { return false; }
+        let read_len = buf.len().min(data.len() - offset);
+        buf[..read_len].clone_from_slice(&data[offset..offset + read_len]);
+        return true;
+    }
+
+    fn write(&self, buf: &[u8], offset: u64) -> bool {
+        let mut vfd = self.vfd.lock();
+        let offset = offset as usize;
+        let write_end = buf.len() + offset;
+        let new_size = write_end.max(vfd.data.len());
+        vfd.data.resize(new_size, 0);
+        vfd.data[offset..write_end].clone_from_slice(buf);
+        vfd.meta.size = new_size as u64;
+        return true;
+    }
+
+    fn truncate(&self, size: u64) -> bool {
+        let mut vfd = self.vfd.lock();
+        vfd.data.resize(size as usize, 0);
+        vfd.meta.size = size;
+        return true;
+    }
+
+    fn list(&self) -> Option<Vec<String>> {
+        return None;
+    }
+
+    fn walk(&self, _: &str) -> Option<Arc<dyn VirtFNode>> {
+        return None;
+    }
+
+    fn create(&self, _: &str, _: Arc<dyn VirtFNode>) -> bool {
+        return false;
+    }
+
+    fn remove(&self, _: &str) -> bool {
+        return false;
+    }
+}
+
+struct VirtDirectory {
+    meta: FMeta,
+    files: Mutex<BTreeMap<String, Arc<dyn VirtFNode>>>
+}
+
+impl VirtDirectory {
+    pub fn new() -> Self {
+        return Self {
+            meta: FMeta::default(vfn::FType::Directory),
+            files: Mutex::new(BTreeMap::new())
+        };
+    }
+}
+
+impl VirtFNode for VirtDirectory {
+    fn meta(&self) -> FMeta {
+        return self.meta.clone();
+    }
+
+    fn read(&self, _: &mut [u8], _: u64) -> bool {
+        return false;
+    }
+
+    fn write(&self, _: &[u8], _: u64) -> bool {
+        return false;
+    }
+
+    fn truncate(&self, _: u64) -> bool {
+        return false;
+    }
+
+    fn list(&self) -> Option<Vec<String>> {
+        return Some(self.files.lock().keys().cloned().collect());
+    }
+
+    fn walk(&self, name: &str) -> Option<Arc<dyn VirtFNode>> {
+        return self.files.lock().get(name).cloned();
+    }
+
+    fn create(&self, name: &str, node: Arc<dyn VirtFNode>) -> bool {
+        let mut files = self.files.lock();
+        if files.contains_key(name) { return false; }
+        files.insert(String::from(name), node);
+        return true;
+    }
+
+    fn remove(&self, name: &str) -> bool {
+        return self.files.lock().remove(name).is_some();
+    }
+}
 
 struct Ramfs {
-    files: BTreeMap<String, Vec<u8>>
+    root: Arc<VirtDirectory>
 }
 
 impl Ramfs {
     pub fn new() -> Self {
-        return Self { files: BTreeMap::new() };
+        return Self { root: Arc::new(VirtDirectory::new()) };
     }
 
-    pub fn create(&mut self, name: &str) -> bool {
-        if self.files.contains_key(name) { return false; }
-        self.files.insert(String::from(name), Vec::new());
-        return true;
+    pub fn read(&self, path: &str, buf: &mut [u8], offset: u64) -> bool {
+        return self.walk(path, false).is_some_and(|file| {
+            file.read(buf, offset)
+        });
     }
 
-    pub fn read(&self, name: &str, buf: &mut [u8], offset: usize) -> bool {
-        let Some(f) = self.files.get(name) else { return false; };
-        if offset >= f.len() { return false; }
-        let read_len = buf.len().min(f.len() - offset);
-        buf[..read_len].clone_from_slice(&f[offset..offset + read_len]);
-        return true;
+    pub fn write(&self, path: &str, buf: &[u8], offset: u64) -> bool {
+        return self.walk(path, false).is_some_and(|file| {
+            file.write(buf, offset)
+        });
     }
 
-    pub fn write(&mut self, name: &str, buf: &[u8], offset: usize) -> bool {
-        let Some(f) = self.files.get_mut(name) else { return false; };
-        let write_end = buf.len() + offset;
-        f.resize(write_end.max(f.len()), 0);
-        f[offset..write_end].clone_from_slice(buf);
-        return true;
+    pub fn truncate(&self, path: &str, size: u64) -> bool {
+        return self.walk(path, false).is_some_and(|file| {
+            file.truncate(size)
+        });
     }
 
-    pub fn truncate(&mut self, name: &str, size: usize) -> bool {
-        let Some(f) = self.files.get_mut(name) else { return false; };
-        f.resize(size, 0);
-        return true;
+    pub fn walk(&self, path: &str, parent: bool) -> Option<Arc<dyn VirtFNode>> {
+        let Ok(mut parts) = get_path_parts(path) else { return None; };
+        if parent { parts.pop(); }
+        let mut current = self.root.clone() as Arc<dyn VirtFNode>;
+        for part in parts {
+            let Some(next) = current.walk(part) else { return None; };
+            current = next;
+        }
+        return Some(current);
     }
 
-    pub fn delete(&mut self, name: &str) -> bool {
-        return self.files.remove(name).is_some();
+    pub fn create(&mut self, path: &str, ftype: FType) -> bool {
+        let Some(dir) = self.walk(path, true) else { return false; };
+        let Some(filename) = get_file_name(path) else { return false; };
+        let node = match ftype {
+            FType::Regular => Arc::new(VirtFile::new()) as Arc<dyn VirtFNode>,
+            FType::Directory => Arc::new(VirtDirectory::new()) as Arc<dyn VirtFNode>,
+        };
+        return dir.create(filename, node);
+    }
+
+    pub fn remove(&mut self, path: &str) -> bool {
+        let Some(dir) = self.walk(path, true) else { return false; };
+        let Some(filename) = get_file_name(path) else { return false; };
+        return dir.remove(filename);
     }
 }
 
 fn get_path_parts(path: &str) -> Result<Vec<&str>, ()> {
-    if !path.starts_with('/') {
-        return Err(());
-    }
+    if !path.starts_with('/') { return Err(()); }
     let mut parts = Vec::new();
     for part in path.split('/').filter(|s| !s.is_empty()) {
         match part {
@@ -58,16 +192,19 @@ fn get_path_parts(path: &str) -> Result<Vec<&str>, ()> {
     return Ok(parts);
 }
 
+fn get_file_name(path: &str) -> Option<&str> {
+    if !path.starts_with('/') { return None; }
+    let name = path.split('/').last()?;
+    if name.is_empty() { return None; }
+    return Some(name);
+}
+
 pub fn init_filesys() {
     let mut filesys = Ramfs::new();
-    let mut buf = "Hello, world!".as_bytes().to_vec();
-    filesys.create("log.txt");
-    filesys.write("log.txt", &buf, 0);
-    filesys.truncate("log.txt", 5);
-    buf.iter_mut().for_each(|x| *x = 0);
-    filesys.write("log.txt", b"!", 5);
-    filesys.read("log.txt", &mut buf, 0);
+    let mut buf = "fn main() {\n    println!(\"Hello, world!\");\n}".as_bytes().to_vec();
+    if !filesys.create("/main.rs", FType::Regular) { return; }
+    if !filesys.write("/main.rs", &buf, 0) { return; }
+    buf.iter_mut().for_each(|b| *b = 0);
+    if !filesys.read("/main.rs", &mut buf, 0) { return; }
     dump_bytes(&buf);
-    filesys.delete("log.txt");
-    printlnk!("/{}", get_path_parts("/a/../../b/c/./d//e/../").unwrap().join("/"));
 }
---
+++ kernel/src/filesys/vfn.rs
@@ -0,0 +1,42 @@
+use alloc::{string::String, sync::Arc, vec::Vec};
+
+#[repr(u8)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum FType {
+    Regular = 0,
+    Directory = 1
+}
+
+#[derive(Clone)]
+pub struct FMeta {
+    pub size: u64,
+    pub ftype: FType,
+    pub perm: u16,
+    pub uid: u16,
+    pub gid: u16
+}
+
+impl FMeta {
+    pub fn default(ftype: FType) -> Self {
+        let perm = match ftype {
+            FType::Regular => 0x644,
+            FType::Directory => 0x755
+        };
+        return Self {
+            size: 0,
+            ftype, perm,
+            uid: 0, gid: 0
+        };
+    }
+}
+
+pub trait VirtFNode {
+    fn meta(&self) -> FMeta;
+    fn read(&self, buf: &mut [u8], offset: u64) -> bool;
+    fn write(&self, buf: &[u8], offset: u64) -> bool;
+    fn truncate(&self, size: u64) -> bool;
+    fn list(&self) -> Option<Vec<String>>;
+    fn walk(&self, name: &str) -> Option<Arc<dyn VirtFNode>>;
+    fn create(&self, name: &str, node: Arc<dyn VirtFNode>) -> bool;
+    fn remove(&self, name: &str) -> bool;
+}
