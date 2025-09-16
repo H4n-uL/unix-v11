1ad8ff12389998acec514f3118d33a8f64cbda30
H4n-uL <jun061119@gmail.com>
2025-09-16 13:43:23 +0900

    UPDATE :: Added partition device

--- kernel/src/filesys/dev.rs
+++ kernel/src/filesys/dev.rs
@@ -81,3 +81,88 @@ impl VirtFNode for DevFile {
         return Err("This is not a directory.".into());
     }
 }
+
+pub struct PartitionDev {
+    dev: Arc<dyn BlockDevice>,
+    start_lba: u64,
+    block_count: u64,
+    meta: FMeta
+}
+
+impl PartitionDev {
+    pub fn new(dev: Arc<dyn BlockDevice>, start_lba: u64, block_count: u64) -> Self {
+        let mut s = Self { dev, start_lba, block_count, meta: FMeta::default(FType::Partition) };
+        s.meta.size = s.total_size();
+        return s;
+    }
+
+    pub fn total_size(&self) -> u64 {
+        self.block_size() * self.block_count()
+    }
+
+    pub fn read_in(&self, buf: &[u8], offset: u64) -> Result<PageAligned, String> {
+        let bs = self.block_size();
+        let (start, end) = (offset / bs, (offset + buf.len() as u64).div_ceil(bs));
+        let mut tempbuf = PageAligned::new(((end - start) * bs) as usize);
+        self.dev.read_block(&mut tempbuf, start)?;
+        return Ok(tempbuf);
+    }
+}
+
+impl BlockDevice for PartitionDev {
+    fn block_size(&self) -> u64 {
+        self.dev.block_size()
+    }
+
+    fn block_count(&self) -> u64 {
+        self.block_count
+    }
+
+    fn read_block(&self, buf: &mut [u8], lba: u64) -> Result<(), String> {
+        self.dev.read_block(buf, lba + self.start_lba)
+    }
+
+    fn write_block(&self, buf: &[u8], lba: u64) -> Result<(), String> {
+        self.dev.write_block(buf, lba + self.start_lba)
+    }
+}
+
+impl VirtFNode for PartitionDev {
+    fn meta(&self) -> FMeta {
+        return self.meta.clone();
+    }
+
+    fn read(&self, buf: &mut [u8], offset: u64) -> Result<(), String> {
+        let offset = offset + self.start_lba * self.block_size();
+        let temp_buf = self.read_in(buf, offset)?;
+        buf.copy_from_slice(&temp_buf[(offset % self.block_size()) as usize..][..buf.len()]);
+        return Ok(());
+    }
+
+    fn write(&self, buf: &[u8], offset: u64) -> Result<(), String> {
+        let offset = offset + self.start_lba * self.block_size();
+        let mut temp_buf = self.read_in(buf, offset)?;
+        temp_buf[(offset % self.block_size()) as usize..][..buf.len()].copy_from_slice(buf);
+        return self.dev.write_block(&temp_buf, offset / self.block_size());
+    }
+
+    fn truncate(&self, _: u64) -> Result<(), String> {
+        return Err("This is not a file.".into());
+    }
+
+    fn list(&self) -> Option<Vec<String>> {
+        return None;
+    }
+
+    fn walk(&self, _: &str) -> Option<Arc<dyn VirtFNode>> {
+        return None;
+    }
+
+    fn create(&self, _: &str, _: Arc<dyn VirtFNode>) -> Result<(), String> {
+        return Err("This is not a directory.".into());
+    }
+
+    fn remove(&self, _: &str) -> Result<(), String> {
+        return Err("This is not a directory.".into());
+    }
+}
