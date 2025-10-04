e1a1330556f3b5952c9ab2c187b4f08eff1551e2
H4n-uL <jun061119@gmail.com>
2025-10-04 10:44:54 +0900

    DEP :: Moved from `nvme-rs` to `nvme-oxide`

--- kernel/src/device/nvme.rs
+++ kernel/src/device/nvme.rs
@@ -5,12 +5,12 @@ use crate::{
 };
 
 use alloc::{collections::btree_map::BTreeMap, format, string::String, sync::Arc};
-use nvme_rs::{Allocator, NVMeDevice};
+use nvme_oxide::{Dma, NVMeDev, Ns};
 use spin::Mutex;
 
 pub struct NVMeAlloc;
 
-impl Allocator for NVMeAlloc {
+impl Dma for NVMeAlloc {
     unsafe fn alloc(&self, size: usize) -> usize {
         return PHYS_ALLOC.alloc(AllocParams::new(size)).unwrap().addr();
     }
@@ -19,69 +19,62 @@ impl Allocator for NVMeAlloc {
         unsafe { PHYS_ALLOC.free_raw(addr as *mut u8, size); }
     }
 
-    fn trans(&self, addr: usize) -> usize { addr }
+    fn virt_to_phys(&self, va: usize) -> usize { va }
 }
 
 pub struct BlockDeviceNVMe {
-    dev: Arc<NVMeDevice<NVMeAlloc>>,
-    devid: u16,
-    nsid: u32
+    ns: Arc<Ns<NVMeAlloc>>,
+    devid: u16
 }
 
 impl BlockDeviceNVMe {
-    pub fn new(dev: Arc<NVMeDevice<NVMeAlloc>>, devid: u16, nsid: u32) -> Self {
-        Self { dev, devid, nsid }
+    pub fn new(ns: Arc<Ns<NVMeAlloc>>, devid: u16) -> Self {
+        Self { ns, devid }
     }
 }
 
 impl BlockDevice for BlockDeviceNVMe {
     fn block_size(&self) -> u64 {
-        return self.dev.get_ns(self.nsid).map_or(0, |ns| ns.block_size());
+        return self.ns.blk_sz() as u64;
     }
 
     fn block_count(&self) -> u64 {
-        return self.dev.get_ns(self.nsid).map_or(0, |ns| ns.block_count());
+        return self.ns.blk_cnt();
     }
 
     fn read_block(&self, buf: &mut [u8], lba: u64) -> Result<(), String> {
-        let ns = self.dev.get_ns(self.nsid)
-            .ok_or_else(|| String::from("Invalid namespace"))?;
-
         let mut pabuf = PageAligned::new(buf.len());
-        ns.read(lba, &mut pabuf).map_err(|e|
-            format!("NVMe read error: {}", e)
+        self.ns.read(lba, &mut pabuf).map_err(|e|
+            format!("NVMe read error: {:?}", e)
         )?;
         buf.copy_from_slice(&pabuf[..buf.len()]);
         return Ok(());
     }
 
     fn write_block(&self, buf: &[u8], lba: u64) -> Result<(), String> {
-        let ns = self.dev.get_ns(self.nsid)
-            .ok_or_else(|| String::from("Invalid namespace"))?;
-
         // PageAligned ensures both address and size alignment to 4 kiB
         // via AllocParams' default settings.
         let mut pabuf = PageAligned::new(buf.len());
         if buf.len() % self.block_size() as usize != 0 {
-            ns.read(lba, &mut pabuf).map_err(|e|
-                format!("NVMe read error: {}", e)
+            self.ns.read(lba, &mut pabuf).map_err(|e|
+                format!("NVMe read error: {:?}", e)
             )?;
         }
         pabuf[..buf.len()].copy_from_slice(buf);
-        return ns.write(lba, &pabuf).map_err(|e|
-            format!("NVMe write error: {}", e)
+        return self.ns.write(lba, &pabuf).map_err(|e|
+            format!("NVMe write error: {:?}", e)
         );
     }
 
     fn devid(&self) -> u64 {
         return DevId::new(0)
             .ty(BlockDevType::PCIe)
-            .loc(((self.devid as u32) << 16) | self.nsid)
+            .loc(((self.devid as u32) << 16) | self.ns.id())
             .build();
     }
 }
 
-pub static NVME_DEV: Mutex<BTreeMap<u16, Arc<NVMeDevice<NVMeAlloc>>>> = Mutex::new(BTreeMap::new());
+pub static NVME_DEV: Mutex<BTreeMap<u16, Arc<NVMeDev<NVMeAlloc>>>> = Mutex::new(BTreeMap::new());
 
 pub fn init_nvme() {
     let mut nvme_devices = NVME_DEV.lock();
@@ -94,10 +87,11 @@ pub fn init_nvme() {
 
         let devid = pci_dev.devid;
         GLACIER.map_range(mmio_addr, mmio_addr, PAGE_4KIB * 2, flags::D_RW);
-        let nvme_arc = NVMeDevice::init(mmio_addr, NVMeAlloc).unwrap();
-        for ns in nvme_arc.list_ns() {
-            block_devices.push(Arc::new(BlockDeviceNVMe::new(nvme_arc.clone(), devid, ns)));
+        if let Ok(nvme) = NVMeDev::new(mmio_addr, NVMeAlloc) {
+            for ns in nvme.ns_list() {
+            block_devices.push(Arc::new(BlockDeviceNVMe::new(ns.clone(), devid)));
+            }
+            nvme_devices.insert(devid, nvme);
         }
-        nvme_devices.insert(devid, nvme_arc.clone());
     }
 }
