1f4d0fe9b59cd51f5090afaddc13204a8f41ccb6
H4n-uL <jun061119@gmail.com>
2025-12-16 00:06:13 +0900

    UPDATE :: Changed spark parameters

--- kernel/src/ram/reloc.rs
+++ kernel/src/ram/reloc.rs
@@ -1,13 +1,16 @@
 use crate::{
     arch::{R_RELATIVE, mmu::flags, move_stack},
-    ram::{KHEAP, PAGE_4KIB, STACK_SIZE, align_up, glacier::GLACIER, physalloc::{AllocParams, PHYS_ALLOC}},
+    ram::{
+        KHEAP, PAGE_4KIB, STACK_SIZE, align_up,
+        glacier::GLACIER, physalloc::{AllocParams, PHYS_ALLOC}
+    },
     sysinfo::{RAMType, RelaEntry, SYS_INFO}
 };
 
 use core::mem::transmute;
 
-static mut SPARK_PTR: usize = 0;
-static mut OLD_KBASE: usize = 0;
+pub static SPARK_PTR: usize = 0;
+pub static OLD_KBASE: usize = 0;
 
 pub fn reloc() -> ! {
     let kinfo;
@@ -27,23 +30,39 @@ pub fn reloc() -> ! {
     SYS_INFO.set_new_stack_base(stack_ptr.addr() + stack_ptr.size());
 
     jump_target = !((1 << (GLACIER.cfg().va_bits - 1)) - 1);
-    GLACIER.map_range(jump_target, new_kbase.addr(), kinfo.size, flags::K_RWO);
-    GLACIER.map_range(jump_target + kinfo.text_ptr, new_kbase.addr() + kinfo.text_ptr, kinfo.text_len, flags::K_ROX);
+
+    // Kernel mapping
+    GLACIER.map_range(
+        jump_target, new_kbase.addr(),
+        kinfo.size, flags::K_RWO
+    );
+    GLACIER.map_range(
+        jump_target + kinfo.text_ptr, new_kbase.addr() + kinfo.text_ptr,
+        kinfo.text_len, flags::K_ROX
+    );
+
+
+    // Kernel base update as physical address
     SYS_INFO.lock().kernel.base = new_kbase.addr();
     let old_kbase = kinfo.base;
     let delta = jump_target - old_kbase;
 
     // KERNEL CLONE
     unsafe {
-        (&raw mut SPARK_PTR).write_volatile(crate::spark as *const () as usize + delta);
-        (&raw mut OLD_KBASE).write_volatile(old_kbase);
-        // KHEAP_VLOC.store(align_up(jump_target + kinfo.size, PAGE_4KIB), AtomOrd::SeqCst);
+        (&raw const SPARK_PTR as *mut usize).write_volatile(crate::spark as *const () as usize + delta);
+        (&raw const OLD_KBASE as *mut usize).write_volatile(old_kbase);
         KHEAP.lock().oom_handler.set_base(align_up(jump_target + kinfo.size, PAGE_4KIB));
+
         (old_kbase as *const u8).copy_to(new_kbase.ptr(), kinfo.size);
     }
-    // EVERY MODIFICATION OF STATIC VARIABLES ARE VOID BEYOND THIS POINT.
+    // ANY MODIFICATION OF STATIC VARIABLES IS VOID BEYOND THIS POINT.
 
-    let rela = unsafe { core::slice::from_raw_parts((kinfo.rela_ptr + old_kbase) as *const RelaEntry, kinfo.rela_len) };
+    let rela = unsafe {
+        core::slice::from_raw_parts(
+            (kinfo.rela_ptr + old_kbase) as *const RelaEntry,
+            kinfo.rela_len
+        )
+    };
 
     // Relocation
     for entry in rela.iter() {
@@ -54,8 +73,9 @@ pub fn reloc() -> ! {
         }
     }
 
+    // JUMP
     unsafe {
-        move_stack(&stack_ptr);
-        transmute::<usize, extern "C" fn(usize) -> !>(SPARK_PTR)(OLD_KBASE);
+        move_stack(&stack_ptr); // ALL STACK VARIABLES ARE VOID BEYOND THIS POINT.
+        transmute::<usize, extern "C" fn() -> !>(SPARK_PTR)();
     }
 }
