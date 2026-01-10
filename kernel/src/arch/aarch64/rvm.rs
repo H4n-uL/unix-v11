29e26a611fd7ccc6096afb46e8edce638c469bfc
H4n-uL <jun061119@gmail.com>
2026-01-10 22:59:14 +0900

    UPDATE :: Added larger VA support and panic on non-4 kiB page

--- kernel/src/arch/aarch64/rvm.rs
+++ kernel/src/arch/aarch64/rvm.rs
@@ -23,20 +23,41 @@ pub mod flags {
 
 impl RvmCfg {
     pub fn detect() -> Self {
-        let psz = PageSize::Size4kiB;
-        let va_bits = 48;
-        let mut mmfr0: usize;
+        let mmfr0: usize;
         unsafe { asm!("mrs {}, ID_AA64MMFR0_EL1", out(reg) mmfr0); }
+
+        let tgran4  = (mmfr0 >> 28) & 0xf;
+        let tgran16 = (mmfr0 >> 20) & 0xf;
+        // let tgran64 = (mmfr0 >> 24) & 0xf;
+
+        let psz = if tgran4 != 0xf {
+            PageSize::Size4kiB
+        // } else if tgran16 != 0 {
+        //     PageSize::Size16kiB
+        // } else if tgran64 != 0xf {
+        //     PageSize::Size64kiB
+        } else {
+            panic!("No supported page granule found");
+        };
+
+        let mmfr2: usize;
+        unsafe { asm!("mrs {}, ID_AA64MMFR2_EL1", out(reg) mmfr2); }
+        let va_range = (mmfr2 >> 16) & 0xf;
+
+        let va_bits = if va_range == 0 {
+            48
+        } else {
+            match psz {
+                PageSize::Size4kiB  if tgran4  != 0x1 => 48,
+                PageSize::Size16kiB if tgran16 != 0x2 => 48,
+                _ => 52
+            }
+        };
+
         let ips = mmfr0 & 0xf;
         let pa_bits = match ips {
-            0 => 32,
-            1 => 36,
-            2 => 40,
-            3 => 42,
-            4 => 44,
-            5 => 48,
-            6 => 52,
-            _ => 48
+            0 => 32, 1 => 36, 2 => 40, 3 => 42,
+            4 => 44, 5 => 48, 6 => 52, _ => 48
         };
 
         return Self { psz, va_bits, pa_bits };
@@ -45,15 +66,15 @@ impl RvmCfg {
     fn tcr_el1(&self) -> usize {
         let tnsz = usize::BITS as usize - self.va_bits as usize;
 
-        let tg = match self.psz {
-            PageSize::Size4kiB => 0b00,
-            PageSize::Size16kiB => 0b10,
-            PageSize::Size64kiB => 0b01
+        let (tg0, tg1) = match self.psz {
+            PageSize::Size4kiB  => (0b00, 0b10),
+            PageSize::Size16kiB => (0b10, 0b01),
+            PageSize::Size64kiB => (0b01, 0b11)
         };
 
         let mut tcr = 0;
         tcr |= tnsz | (tnsz << 16); // T0SZ and T1SZ
-        tcr |= (tg << 14) | (tg << 30); // TG0 and TG1
+        tcr |= (tg0 << 14) | (tg1 << 30); // TG0 and TG1
 
         tcr |= 0b01 << 8;  // IRGN0 = Normal WB/WA
         tcr |= 0b01 << 10; // ORGN0 = Normal WB/WA
